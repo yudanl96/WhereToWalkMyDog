150110798488932b2f2ed49f7b38007795b2b3b8
Yudan Liu <liuyudan24@gmail.com>
2024-09-03 22:01:21 -0500

    seed generated

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,29 @@
 const mongoose = require('mongoose');
-const Place = require('./models/place');
+const Place = require('../models/place');
+const cities = require('./cities');
+const {adj, placeType} = require('./titles');
 
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/walkMyDog');
+  }
+  
+main()
+.then(()=>console.log('Connected to monogo'))
+.catch(err => console.log(err));
 
+const sample = (inputs) => inputs[Math.floor(Math.random() * inputs.length)];
+
+const seedDB = async()=>{
+    await Place.deleteMany();
+    for (let index = 0; index < 50; index++) {
+        const rand = Math.floor(Math.random()*1000)
+        const place = new Place({
+            location: `${cities[rand].city}, ${cities[rand].state}`,
+            name: `${sample(adj)} ${sample(placeType)}`,
+            leash: sample([true, false])
+        });
+        await place.save()
+    }
+}
+
+seedDB();
---
+++ seeds/titles.js
@@ -0,0 +1,28 @@
+module.exports.adj = [
+    'Clean',
+    'Dog Friendly',
+    'Safe',
+    'Easily Accessible',
+    'Open 24/7',
+    'Day-time',
+    'Crowded',
+    'Fenced',
+    'Empty',
+    'Peaceful',
+    'Busy',
+    'Spacious',
+    'Social',
+    'Scenary'
+]
+
+module.exports.placeType  = [
+    'Downtown Walk',
+    'Lakeside Walk',
+    'Lawn',
+    'Forest',
+    'Paved Road',
+    'Enclosed Space',
+    'Trail',
+    'Park'
+]
+
